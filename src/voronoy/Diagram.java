1d7566b2d96531fe355de0f91f49b90e332f051b
alexshavlovsky <ctzn@tut.by>
2018-10-01 23:03:28 +0300

    visualisation improved

--- src/voronoy/Diagram.java
+++ src/voronoy/Diagram.java
@@ -8,15 +8,15 @@ import java.util.stream.Stream;
 
 import static voronoy.ConvexHull.mergeHulls;
 
-class Diagram{
-    final static private int Y_EXPORT=675;
+class Diagram {
+    final static private int Y_EXPORT = 675;
     private ConvexHull hull;
-    private HashMap<Point2D,Set<Edge>> siteEdge = new HashMap<>();      //0-...
-    private HashMap<Point2D,Set<Point2D>> siteNode = new HashMap<>();   //0-...
-    private HashMap<Point2D,Set<Point2D>> nodeSite = new HashMap<>();   //3
-    private HashMap<Point2D,Set<Edge>> nodeEdge = new HashMap<>();      //3
-    private HashMap<Edge,Point2D[]> edgeSite = new HashMap<>();      //2
-    private HashMap<Edge,Set<Point2D>> edgeNode = new HashMap<>();      //0-2
+    private HashMap<Point2D, Set<Edge>> siteEdge = new HashMap<>();      //0-...
+    private HashMap<Point2D, Set<Point2D>> siteNode = new HashMap<>();   //0-...
+    private HashMap<Point2D, Set<Point2D>> nodeSite = new HashMap<>();   //3
+    private HashMap<Point2D, Set<Edge>> nodeEdge = new HashMap<>();      //3
+    private HashMap<Edge, Point2D[]> edgeSite = new HashMap<>();         //2
+    private HashMap<Edge, Set<Point2D>> edgeNode = new HashMap<>();      //0-2
 
     @Override
     public String toString() {
@@ -53,11 +53,11 @@ class Diagram{
     }
 
     public Diagram(int x1, int y1) {
-        this(new Point2D(x1,Y_EXPORT-y1));
+        this(new Point2D(x1, Y_EXPORT - y1));
     }
 
     public Diagram(int x1, int y1, int x2, int y2) {
-        this(new Point2D(x1,Y_EXPORT-y1),new Point2D(x2,Y_EXPORT-y2));
+        this(new Point2D(x1, Y_EXPORT - y1), new Point2D(x2, Y_EXPORT - y2));
     }
 
     public void consolidate(Diagram d2) {
@@ -90,17 +90,21 @@ class Diagram{
     public Object[] getFirstIntersect(Point2D p1, Point2D p2, double ym) {
         LineCommon ray = new Edge(p1, p2);
         return Stream.of(
-                siteEdge.get(p1).stream().map(e->new Object[]{ray.getIntersectionPoint(e),p1,e}),
-                siteEdge.get(p2).stream().map(e->new Object[]{ray.getIntersectionPoint(e),p2,e})
-        ).flatMap(x -> x).filter(p -> ((Point2D)p[0]).y + ZERO < ym).max((a, b) -> (int) (((Point2D)a[0]).y - ((Point2D)b[0]).y)).orElse(null);
+                siteEdge.get(p1).stream().map(e -> new Object[]{ray.getIntersectionPoint(e), p1, e}),
+                siteEdge.get(p2).stream().map(e -> new Object[]{ray.getIntersectionPoint(e), p2, e})
+        ).flatMap(x -> x).filter(p -> ((Point2D) p[0]).y + ZERO < ym).max((a, b) -> (int) (((Point2D) a[0]).y - ((Point2D) b[0]).y)).orElse(null);
     }
 
-    Point2D getOpposite(Edge e, Point2D p){
+    Point2D getOpposite(Edge e, Point2D p) {
         Point2D[] pair = edgeSite.get(e);
-        return pair[0]==p?pair[1]:pair[0];
+        return pair[0] == p ? pair[1] : pair[0];
     }
 
-    static ArrayList<Point2D> nodes = new ArrayList<>();
+    static ArrayList<Object[]> nodes = new ArrayList<>();
+
+    static Color getColorFromInt(int i) {
+        return Color.getHSBColor((i % 12) / 12.0f, 0.8f, 0.8f);
+    }
 
     static Diagram mergeDiagrams(Diagram d1, Diagram d2) {
         ConvexHull h = mergeHulls(d1.hull, d2.hull);
@@ -113,7 +117,7 @@ class Diagram{
         while (true) {
             Object[] intersect = d.getFirstIntersect(p1, p2, ym);
             if (intersect == null) break;
-            nodes.add((Point2D) intersect[0]);
+            nodes.add(intersect);
             if (intersect[1] == p1) p1 = d1.getOpposite((Edge) intersect[2], p1);
             if (intersect[1] == p2) p2 = d2.getOpposite((Edge) intersect[2], p2);
             ym = ((Point2D) intersect[0]).y;
@@ -122,10 +126,13 @@ class Diagram{
     }
 
     void draw(Canvas pap) {
-        for (int i = 0; i < nodes.size()-1; i++) {
-            pap.addLine(new Line2D(nodes.get(i),nodes.get(i+1)),Color.ORANGE);
+        for (int i = 0; i < nodes.size(); i++) {
+            pap.addPoint((Point2D) nodes.get(i)[0], getColorFromInt(i), 8);
+            pap.addPoint((Point2D) nodes.get(i)[1], getColorFromInt(i), 32);
         }
-
+        for (int i = 0; i < nodes.size() - 1; i++) {
+            pap.addLine(new Line2D((Point2D) nodes.get(i)[0], (Point2D) nodes.get(i + 1)[0]), getColorFromInt(i));
+        }
         for (Map.Entry<Point2D, Set<Edge>> point2DSetEntry : siteEdge.entrySet()) {
             pap.addPoint(point2DSetEntry.getKey(), Color.RED, 16);
             for (Edge edge : point2DSetEntry.getValue())
@@ -133,7 +140,7 @@ class Diagram{
         }
     }
 
-    String exportSites(){
+    String exportSites() {
         return null;//points.keySet().stream().map(p->(int)p.x+","+(Y_EXPORT-(int)p.y)).collect(Collectors.joining(",","{\"sites\":[","],\"queries\":[]}"));
     }
 
--- src/voronoy/Voron.java
+++ src/voronoy/Voron.java
@@ -33,9 +33,11 @@ public class Voron {
 
         Diagram d1 = new Diagram(145, 475, 248, 209);
         Diagram d2 = new Diagram(289, 397, 442, 468);
-//        Diagram d3 = new Diagram(490,380);
-
         Diagram d = mergeDiagrams(d1, d2);
+
+//        Diagram d3 = new Diagram(490,380);
+//        Diagram d = mergeDiagrams(d1, d3);
+
         System.out.println(d);
         d.draw(paper);
 //        System.out.println(d1.exportSites());
